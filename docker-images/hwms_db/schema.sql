55fa2e727bd9d97a716c4e9caa23162c5b86d2b2
mattjlewis <matthew.lewis@btinternet.com>
2017-05-22 08:43:32 +0100

    Fixing privileges

--- docker-images/hwms_db/schema.sql
+++ docker-images/hwms_db/schema.sql
@@ -1,9 +1,9 @@
 CREATE USER IF NOT EXISTS 'hwms_owner'@'%' IDENTIFIED BY 'helloworld';
 CREATE USER IF NOT EXISTS 'hwms_owner'@'localhost' IDENTIFIED BY 'helloworld';
 CREATE DATABASE IF NOT EXISTS hwms;
+GRANT ALL PRIVILEGES ON hwms.* TO 'hwms_owner'@'%';
+GRANT ALL PRIVILEGES ON hwms.* TO 'hwms_owner'@'localhost';
 use hwms
-GRANT ALL PRIVILEGES ON hwms TO 'hwms_owner'@'%';
-GRANT ALL PRIVILEGES ON hwms TO 'hwms_owner'@'localhost';
 
 CREATE TABLE users (
 	id MEDIUMINT not null auto_increment primary key,
