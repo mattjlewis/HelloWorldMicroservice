efbbcdf28a3706cdf1954ac8e752abfd9c89265f
mattjlewis <matthew.lewis@btinternet.com>
2017-05-22 08:35:34 +0100

    Schema privileges

--- docker-images/hwms_db/schema.sql
+++ docker-images/hwms_db/schema.sql
@@ -1,7 +1,9 @@
-CREATE USER IF NOT EXISTS hwms_owner IDENTIFIED BY 'helloworld';
+CREATE USER IF NOT EXISTS 'hwms_owner'@'%' IDENTIFIED BY 'helloworld';
+CREATE USER IF NOT EXISTS 'hwms_owner'@'localhost' IDENTIFIED BY 'helloworld';
 CREATE DATABASE IF NOT EXISTS hwms;
 use hwms
-GRANT ALL ON hwms TO 'hwms_owner';
+GRANT ALL PRIVILEGES ON hwms TO 'hwms_owner'@'%';
+GRANT ALL PRIVILEGES ON hwms TO 'hwms_owner'@'localhost';
 
 CREATE TABLE users (
 	id MEDIUMINT not null auto_increment primary key,
